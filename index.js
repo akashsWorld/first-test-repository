3199d8d5308cd007a5afdb1febc91a21e4b7d003
akashsWorld <akashbiswas.271999@gmail.com>
2024-01-17 23:00:53 +0530

    Function added

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ console.log("Hey this is fine, some dummy text");
 console.log("Hey this is fine, some dummy text");
 console.log("Hey this is fine, some dummy text");
 
+let func = (someText )=>{
+    console.log(someText);
+}
+
+
+
 
 
 let a = "Some text written here."
